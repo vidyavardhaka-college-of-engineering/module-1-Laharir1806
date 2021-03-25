6a4d85d97e96fc2070447904e5957ca9ab12c0f7
Laharir1806 <lahariravishankara01@gmail.com>
2021-03-25 08:39:16 +0000

    done

--- Program-8.c
+++ Program-8.c
@@ -5,11 +5,12 @@ int main()
 {
     int x, y;
     printf(" Enter the value of x: ");
+    scanf("%d", &x);
     // insert the missing code
-    printf("y = %d  ",y=++x)
-    printf("y = %d  ",y=x++)
-    printf("y = %d  ",y=--x)
-    printf("y = %d  ",y=x--) // correct the code 
+    printf("y = %d  ",y=++x);
+    printf("y = %d  ",y=x++);
+    printf("y = %d  ",y=--x);
+    printf("y = %d  ",y=x--); // correct the code 
     return 0;
 }
 
